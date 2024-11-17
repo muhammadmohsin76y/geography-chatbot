22dc376a8fee1c11132b13aec64d5c6b48a980a0
muhammadmohsin76y <muhammadmohsin76y@gmail.com>
2024-11-17 17:51:08 +0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [];
+    },
+  };
+  
+  export default nextConfig;
